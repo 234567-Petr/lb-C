a654ecc23310442a15de3319f89293eb5a2ef27c
234567-Petr <zuravlevpetr197@gmail.com>
2024-11-13 19:17:38 +0300

    Added a fool check

--- Laba2.1.c
+++ Laba2.1.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 
-double calculateSum(double E);
-
+double calculate(double E);
+double Proverka();
 int main() {
-    double E;
-    printf("\nВведите точность E: ");
-    scanf("%lf", &E); 
-
-    double result = calculateSum(E);
-    printf("Сумма ряда: %lf\n", result);
+    double E = Proverka();
+    double itog = calculate(E);
+    printf("Сумма ряда: %lf\n", itog);
 
     return 0;
 }
 
-double calculateSum(double E) {
-    double firstNum = 1.0; 
+double calculate(double E) {
+    double Number = 1.0; 
     double sum = 0;
     int n = 1;
-
-    while (firstNum > E) {
-        firstNum /= n;
-        sum += firstNum;
+    
+    while (Number >= E) {
+        Number /= n;
+        sum += Number;
         n++;
     }
 
     return sum;
 }
+double Proverka(){
+    double ros;
+    printf("Введите значение: ");
+    if (!(scanf("%lf", &ros))){
+        printf("Ввод не коректен");
+        return -1;
+    }
+    else{
+        return ros > 0? ros: -1;
+    }
+
+}   
